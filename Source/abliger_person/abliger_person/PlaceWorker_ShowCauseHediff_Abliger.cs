75d7127420a096206b572553bd997dc20df45fc4
abliger <3330181534@qq.com>
2024-01-19 12:57:29 +0800

    style: 添加忽略文件，建立正确映射

--- Source/abliger_person/abliger_person/PlaceWorker_ShowCauseHediff_Abliger.cs
+++ Source/abliger_person/abliger_person/PlaceWorker_ShowCauseHediff_Abliger.cs
@@ -4,7 +4,6 @@ namespace RimWorld
 {
     public class PlaceWorker_ShowCauseHediff_Abliger : PlaceWorker
     {
-        // Token: 0x06009B5A RID: 39770 RVA: 0x00384BD8 File Offset: 0x00382DD8
         public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
         {
             CompProperties_Abliger compProperties = def.GetCompProperties<CompProperties_Abliger>();
