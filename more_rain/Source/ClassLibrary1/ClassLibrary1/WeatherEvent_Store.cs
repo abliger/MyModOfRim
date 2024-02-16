1f30c87dd9f719259556f7efc8cc4575c3e10e59
abliger <3330181534@qq.com>
2024-02-16 13:40:26 +0800

    添加砖雨

---
+++ more_rain/Source/ClassLibrary1/ClassLibrary1/WeatherEvent_Store.cs
@@ -0,0 +1,76 @@
+﻿using Mono.Unix.Native;
+using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using UnityEngine.UIElements;
+using Verse;
+
+namespace abliger
+{
+    public class WeatherEvent_Store : WeatherEvent
+    {
+      
+        public WeatherEvent_Store(Map map) : base(map)
+        {
+           this.list= this.map.AllCells.ToList<IntVec3>();
+           Scribe_Defs.Look<DamageDef>(ref this.damageDef, "CrushingInjury");
+        }
+
+        public override bool Expired => age> Rand.Range(1200, 4000);
+        public int age = default;
+        public List<IntVec3> list;
+        public DamageDef damageDef;
+
+        public override void FireEvent()
+        {
+            
+        }
+
+        public override void WeatherEventTick()
+        {
+            this.age++;
+            if(this.age % 200 == 0)
+            {
+            
+            Thing thing = ThingMaker.MakeThing(ThingDefOf.BlocksGranite, null);
+            thing.SetForbidden(true);
+            thing.stackCount = 10;
+                
+            GenSpawn.Spawn(thing, RandomPostionToDamage(RandomPostion(default)) , this.map, WipeMode.FullRefund);
+            }
+        }
+        public IntVec3 RandomPostion(IntVec3 position)
+        {
+            if(position == default)
+            {
+                position = list[new Random().Next(0, list.Count)];
+            }
+            if(!position.Roofed(this.map) && position.InBounds(this.map) && position.Standable(this.map) 
+                && position.GetFirstBuilding(this.map) == null
+                //position.GetFirstItem(this.map) == null || ThingDefOf.BlocksGranite == position.GetFirstItem(this.map).def
+            )
+            {
+                return position;
+            }
+            else
+            {
+                return RandomPostion(default);
+            }
+        }
+
+        public IntVec3 RandomPostionToDamage(IntVec3 position)
+        {
+
+            Pawn pawn = position.GetFirstPawn(this.map);
+            if (pawn != null)
+            {
+                pawn.TakeDamage(new DamageInfo(this.damageDef,30f));
+            }
+            return position;
+        }
+    }
+}
+    
---
+++ more_rain/Source/ClassLibrary1/ClassLibrary1/WeatherOverlay_StoreRain.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace abliger
+{
+    [StaticConstructorOnStartup]
+    public class WeatherOverlay_StoreRain : SkyOverlay
+    {
+        public WeatherOverlay_StoreRain()
+        {
+            LongEventHandler.ExecuteWhenFinished(() =>
+            {
+                this.worldOverlayMat = MaterialPool.MatFrom(ContentFinder<Texture2D>.Get("Weather/StoreOverlayRain", true), ShaderDatabase.Transparent, Color.white);
+                this.worldOverlayPanSpeed1 = 0.015f;
+                this.worldPanDir1 = new Vector2(-0.25f, -1f);
+                this.worldPanDir1.Normalize();
+                this.worldOverlayPanSpeed2 = 0.022f;
+                this.worldPanDir2 = new Vector2(-0.24f, -1f);
+                this.worldPanDir2.Normalize();
+            });
+        }
+    }
+}
+    
